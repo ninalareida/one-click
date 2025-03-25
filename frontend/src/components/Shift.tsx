174539ab48e4a67f7025a76d083b3f20b316f552
lareidan <nina.lareida@roche.com>
2025-03-25 11:48:13 +0100

    dragging animation

--- frontend/src/components/Shift.tsx
+++ frontend/src/components/Shift.tsx
@@ -3,13 +3,12 @@ import { ShiftProps } from '../types/component.types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock, faMoon, faMugSaucer, faSun } from '@fortawesome/free-solid-svg-icons';
 import { useDraggable, useDroppable } from '@dnd-kit/core';
+import { motion } from 'framer-motion';
 
 // Das Shift Component dient der Anzeige jeder Schicht mit dem jeweiligen Namen und Schichttyp.
 const Shift = (props: ShiftProps) => {
-  console.log('Shift props:', props);
-
   // Make shift draggable
-  const { attributes, listeners, setNodeRef, isDragging } = useDraggable({
+  const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
     id: props.id.toString(), // Ensure ID is a string for drag context
   });
 
@@ -32,20 +31,36 @@ const Shift = (props: ShiftProps) => {
   };
 
   return (
-    <div
+    <motion.div
       ref={(node) => {
-        setNodeRef(node); 
+        setNodeRef(node);
         dropRef(node);
       }}
       {...listeners}
       {...attributes}
       className={`shift ${isDragging ? 'dragging' : ''}`}
       data-type={props.shiftType}
+      style={{
+        zIndex: isDragging ? 999 : 'auto',
+        cursor: isDragging ? 'grabbing' : 'grab',
+      }}
+      animate={{
+        x: transform ? transform.x : 0, 
+        y: transform ? transform.y : 0, 
+        scale: isDragging ? 1.15 : 1,
+        opacity: isDragging ? 0.9 : 1, 
+      }}
+      transition={{ 
+        type: "spring", 
+        stiffness: 400,
+        damping: 15,
+        mass: 0.5
+      }}
     >
       <FontAwesomeIcon icon={getIconForShiftType()} className="shift-icon" />
       <p className="employee-name">{props.name}</p>
       <p className="shift-type">{props.shiftType}</p>
-    </div>
+    </motion.div>
   );
 };
 
